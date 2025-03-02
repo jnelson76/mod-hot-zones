d1cf987352f93800c3a2df71f6d4c2371acb450c
jnelson76 <jnelson76@gmail.com>
2025-03-02 07:05:51 -0600

    111

--- src/hot_zones_loader.cpp
+++ src/hot_zones_loader.cpp
@@ -3,20 +3,9 @@
 
 extern void AddSC_HotZones();
 
-class HotZonesLoader : public ModuleScript
-{
-public:
-    HotZonesLoader() : ModuleScript("HotZonesLoader") {}
-
-    void OnLoad() override
-    {
-        if (!sConfigMgr->LoadModuleConfig("hot_zones.conf"))
-            sLog->outError("HotZones", "Failed to load hot_zones.conf");
-    }
-};
-
 void Addmod_hot_zonesScripts()
 {
-    new HotZonesLoader();
+    if (!sConfigMgr->LoadModuleConfig("hot_zones.conf"))
+        sLog->outError("HotZones", "Failed to load hot_zones.conf");
     AddSC_HotZones();
 }
